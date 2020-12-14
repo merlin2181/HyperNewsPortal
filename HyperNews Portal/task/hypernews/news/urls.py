2100c8275aed4fce59bb9d3274d767c25a9b22de
merlin2181 <merlin2181@gmail.com>
2020-12-14 14:43:27 -0800

    Final Stage of HyperNews Portal project from JetBrains Academy
    - Added ability to search news articles
    - Added two pages for content
    - Created a forms.py file for the search function
    - Added a base css file to give site some window dressing
    - Updated program to include a static folder for css, other files, etc.

--- HyperNews Portal/task/hypernews/news/urls.py
+++ HyperNews Portal/task/hypernews/news/urls.py
@@ -1,10 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
-from .views import MainIndex
+from .views import MainIndex, NewsCreate
 
 app_name = 'news'
 urlpatterns = [
     path('', MainIndex.as_view(), name='index'),
     path('news/', views.news_main, name='main_index'),
     path('news/<int:link>/', views.news_detail, name='news_detail'),
+    path('news/create/', NewsCreate.as_view(), name='create'),
 ]
+
+urlpatterns += static(settings.STATIC_URL)
--- HyperNews Portal/task/hypernews/news/views.py
+++ HyperNews Portal/task/hypernews/news/views.py
@@ -1,29 +1,75 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
+from datetime import datetime
 from itertools import groupby
+from .forms import SearchForm
 import json
-
-
-with open(settings.NEWS_JSON_PATH, 'r') as file:
-    news = json.load(file)
+import random
 
 
 class MainIndex(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("<h1>Coming soon</h1>")
+        return redirect('/news/')
+
+
+class NewsCreate(View):
+    with open(settings.NEWS_JSON_PATH, 'r') as file:
+        news = json.load(file)
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'news/news_create.html',)
+
+    def post(self, request, *args, **kwargs):
+        random.seed()
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        create = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        link = random.randint(4,999999)
+        flag = True
+        while flag:
+            link_list = []
+            for item in self.news:
+                link_list.append(item['link'])
+            if link in link_list:
+                random.seed()
+                link = random.randint(4, 999999)
+            else:
+                flag = False
+        story = {'created': create, 'text': text, 'title': title, 'link': link}
+        self.news.append(story)
+        with open(settings.NEWS_JSON_PATH, 'w') as file:
+            json.dump(self.news, file)
+        return redirect('/news/')
 
 
 def news_main(request):
+    with open(settings.NEWS_JSON_PATH, 'r') as file:
+        news = json.load(file)
     sorted_news = sorted(news, key=lambda x: x['created'], reverse=True)
+    query = SearchForm()
+    search = request.GET
+    if search:
+        results = []
+        for item in sorted_news:
+            if search['q'].lower() in item['title'].lower():
+                results.append(item)
+        if results:
+            results_dict = {}
+            for date, content in groupby(results, key=lambda x: x['created'].split()[0]):
+                results_dict[date] = list(content)
+            return render(request, 'news/news_search.html', {'news': results_dict})
+        return render(request, 'news/search_error.html')
     news_dict = {}
     for date, content in groupby(sorted_news, key=lambda x: x['created'].split()[0]):
         news_dict[date] = list(content)
-    return render(request, 'news/news_list.html', {'news':news_dict})
+    return render(request, 'news/news_list.html', {'news': news_dict, 'form': query})
 
 
 def news_detail(request, link):
+    with open(settings.NEWS_JSON_PATH, 'r') as file:
+        news = json.load(file)
     for item in news:
         if item['link'] == link:
             article = item
